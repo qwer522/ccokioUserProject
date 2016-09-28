59be09fbd43401a58c33c6ed668e1624d35bdd22
qwer522 <wldnjssla111@naver.com>
2016-09-28 15:48:44 +0900

    비회원 회원 등록완성

--- src/domain/NonUser.java
+++ src/domain/NonUser.java
@@ -5,26 +5,27 @@ public class NonUser {
 	// private int nonUserNumber;
 	private String nonUserName;
 	private String nonUserTel;
-
+	private String nonUserAddress;
 	public NonUser() {
 
 	}
 
-	public NonUser(String nonUserName, String nonUserTel) {
+	public NonUser(String nonUserName, String nonUserTel, String nonUserAddress) {
 		this.nonUserName = nonUserName;
 		this.nonUserTel = nonUserTel;
+		this.nonUserAddress = nonUserAddress;
 	}
-
-	// public int getNonUserNumber() {
-	// return nonUserNumber;
-	// }
-	//
-	// public void setNonUserNumber(int nonUserNumber) {
-	// this.nonUserNumber = nonUserNumber;
-	// }
 
 	public String getNonUserName() {
 		return nonUserName;
+	}
+
+	public String getNonUserAddress() {
+		return nonUserAddress;
+	}
+
+	public void setNonUserAddress(String nonUserAddress) {
+		this.nonUserAddress = nonUserAddress;
 	}
 
 	public void setNonUserName(String nonUserName) {
--- src/view/NonUserRegisterView.java
+++ src/view/NonUserRegisterView.java
@@ -20,14 +20,16 @@ public class NonUserRegisterView {
 
 		String nonUserName = null;
 		String nonUserTel = null;
-
+		String nonUserAddress = null;
+		
 		System.out.println("[비회원 주문 모드]");
-		System.out.print("이름 : ");
+		System.out.print("이   름 : ");
 		nonUserName = keyboard.next();
 		System.out.print("전화번호 : ");
 		nonUserTel = keyboard.next();
-
-		NonUser newNonUser = new NonUser(nonUserName, nonUserTel);
+		System.out.print("주   소 : ");
+		nonUserAddress = keyboard.next();
+		NonUser newNonUser = new NonUser(nonUserName, nonUserTel, nonUserAddress);
 
 		Controllers.getNonUserController().requestReturnNonUserRegister(newNonUser);
 	}
